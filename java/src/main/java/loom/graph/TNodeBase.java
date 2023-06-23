15fc6deda27eea1d3ea438665a962039ce15aea1
Crutcher Dunnavant <crutcher@gmail.com>
2023-06-23 11:35:14 -0700

    display options

--- java/src/main/java/loom/graph/TNodeBase.java
+++ java/src/main/java/loom/graph/TNodeBase.java
@@ -1,73 +1,78 @@
 package loom.graph;
 
 import com.fasterxml.jackson.annotation.*;
+import java.lang.annotation.*;
+import java.util.UUID;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import loom.common.HasToJsonString;
 import loom.common.IdUtils;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-import java.lang.annotation.*;
-import java.util.UUID;
-
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "@type")
 @JsonSubTypes(
-        value = {
-                @JsonSubTypes.Type(value = TTagBase.class),
-                @JsonSubTypes.Type(value = TOperatorBase.class),
-                @JsonSubTypes.Type(value = TSequencePoint.class),
-        })
+    value = {
+      @JsonSubTypes.Type(value = TTagBase.class),
+      @JsonSubTypes.Type(value = TOperatorBase.class),
+      @JsonSubTypes.Type(value = TSequencePoint.class),
+    })
 @TNodeBase.DisplayOptions.BackgroundColor("#ffffff")
 public abstract class TNodeBase implements HasToJsonString {
-    public static class DisplayOptions {
-        private DisplayOptions() {
-        }
+  public static class DisplayOptions {
+    @Nullable public final String backgroundColor;
 
-        @Inherited
-        @Retention(RetentionPolicy.RUNTIME)
-        @Target(ElementType.TYPE)
-        public @interface BackgroundColor {
-            String value() default "";
-        }
+    public DisplayOptions(Class<? extends TNodeBase> cls) {
+      var bg = cls.getAnnotation(BackgroundColor.class).value();
+      if (bg == null || bg.isEmpty()) {
+        backgroundColor = null;
+      } else {
+        backgroundColor = bg;
+      }
     }
 
-    @Nullable
-    @JsonIgnore
-    TGraph graph = null;
-
-    @Nonnull
-    @JsonProperty(required = true)
-    public final UUID id;
-
-    TNodeBase(@Nullable UUID id) {
-        this.id = IdUtils.coerceUUID(id);
+    @Inherited
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    public @interface BackgroundColor {
+      String value() default "";
     }
+  }
 
-    TNodeBase() {
-        this(null);
+  @Nullable @JsonIgnore TGraph graph = null;
+
+  @Nonnull
+  @JsonProperty(required = true)
+  public final UUID id;
+
+  TNodeBase(@Nullable UUID id) {
+    this.id = IdUtils.coerceUUID(id);
+  }
+
+  TNodeBase() {
+    this(null);
+  }
+
+  public DisplayOptions displayOptions() {
+    return new DisplayOptions(getClass());
+  }
+
+  public String jsonTypeName() {
+    return getClass().getAnnotation(JsonTypeName.class).value();
+  }
+
+  public final boolean hasGraph() {
+    return graph != null;
+  }
+
+  public final TGraph assertGraph() {
+    if (graph == null) {
+      throw new IllegalStateException("Node is not part of a graph");
     }
+    return graph;
+  }
 
-    public String displayBackgroundColor() {
-        return getClass().getAnnotation(DisplayOptions.BackgroundColor.class).value();
-    }
+  public void validate() {
+    assertGraph();
+  }
 
-    public String jsonTypeName() {
-        return getClass().getAnnotation(JsonTypeName.class).value();
-    }
-
-    public final boolean hasGraph() {
-        return graph != null;
-    }
-
-    public final TGraph assertGraph() {
-        if (graph == null) {
-            throw new IllegalStateException("Node is not part of a graph");
-        }
-        return graph;
-    }
-
-    public void validate() {
-        assertGraph();
-    }
-
-    public abstract TNodeBase copy();
+  public abstract TNodeBase copy();
 }
--- java/src/main/java/loom/graph/export/TGraphDotExporter.java
+++ java/src/main/java/loom/graph/export/TGraphDotExporter.java
@@ -8,11 +8,6 @@ import guru.nidi.graphviz.engine.Format;
 import guru.nidi.graphviz.engine.Graphviz;
 import guru.nidi.graphviz.model.Factory;
 import guru.nidi.graphviz.model.Graph;
-import java.awt.image.BufferedImage;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.experimental.SuperBuilder;
 import loom.common.JsonUtil;
@@ -20,133 +15,141 @@ import loom.graph.TEdgeBase;
 import loom.graph.TGraph;
 import loom.graph.TTagBase;
 
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
 @SuperBuilder
 public final class TGraphDotExporter {
-  @Builder.Default private boolean enableNodeIds = false;
+    @Builder.Default
+    private boolean enableNodeIds = false;
 
-  public TGraphDotExporter() {}
-
-  public BufferedImage toImage(TGraph tgraph) {
-    return Graphviz.fromGraph(toGraph(tgraph)).render(Format.PNG).toImage();
-  }
-
-  public Graph toGraph(TGraph tgraph) {
-    Graph g =
-        Factory.graph("G")
-            .directed()
-            .graphAttr()
-            .with(Rank.dir(Rank.RankDir.RIGHT_TO_LEFT))
-            .graphAttr()
-            .with("nodesep", "0.7");
-
-    Map<UUID, String> syms = new HashMap<>();
-    for (var tnode : tgraph) {
-      String nodeTypePrefix;
-      if (tnode instanceof TEdgeBase) {
-        nodeTypePrefix = "E";
-      } else if (tnode instanceof TTagBase) {
-        nodeTypePrefix = "T";
-      } else {
-        nodeTypePrefix = "N";
-      }
-      var sym =
-          String.format(
-              "%s.%s%s",
-              tnode.jsonTypeName(), nodeTypePrefix, Integer.toHexString(syms.size() + 1));
-      syms.put(tnode.id, sym);
+    public TGraphDotExporter() {
     }
 
-    for (var tnode : tgraph) {
-      Map<String, Object> data = JsonUtil.toMap(tnode);
-      data.remove("@type");
-      if (!enableNodeIds) {
-        data.remove("id");
+    public BufferedImage toImage(TGraph tgraph) {
+        return Graphviz.fromGraph(toGraph(tgraph)).render(Format.PNG).toImage();
+    }
 
-        if (tnode instanceof TTagBase<?>) {
-          data.remove("sourceId");
+    public Graph toGraph(TGraph tgraph) {
+        Graph g =
+                Factory.graph("G")
+                        .directed()
+                        .graphAttr()
+                        .with(Rank.dir(Rank.RankDir.RIGHT_TO_LEFT))
+                        .graphAttr()
+                        .with("nodesep", "0.7");
+
+        Map<UUID, String> syms = new HashMap<>();
+        for (var tnode : tgraph) {
+            String nodeTypePrefix;
+            if (tnode instanceof TEdgeBase) {
+                nodeTypePrefix = "E";
+            } else if (tnode instanceof TTagBase) {
+                nodeTypePrefix = "T";
+            } else {
+                nodeTypePrefix = "N";
+            }
+            var sym =
+                    String.format(
+                            "%s.%s%s",
+                            tnode.jsonTypeName(), nodeTypePrefix, Integer.toHexString(syms.size() + 1));
+            syms.put(tnode.id, sym);
         }
-        if (tnode instanceof TEdgeBase<?, ?>) {
-          data.remove("targetId");
+
+        for (var tnode : tgraph) {
+            Map<String, Object> data = JsonUtil.toMap(tnode);
+            data.remove("@type");
+            if (!enableNodeIds) {
+                data.remove("id");
+
+                if (tnode instanceof TTagBase<?>) {
+                    data.remove("sourceId");
+                }
+                if (tnode instanceof TEdgeBase<?, ?>) {
+                    data.remove("targetId");
+                }
+            }
+
+            Map<String, String> tableAttrs =
+                    new HashMap<>(
+                            Map.of(
+                                    "border", "0",
+                                    "cellborder", "1",
+                                    "cellspacing", "0",
+                                    "cellpadding", "4"));
+
+            var displayOptions = tnode.displayOptions();
+            if (displayOptions.backgroundColor != null) {
+                tableAttrs.put("bgcolor", displayOptions.backgroundColor);
+            }
+
+            String title = syms.get(tnode.id);
+
+            var attrs =
+                    tableAttrs.entrySet().stream()
+                            .map(e -> e.getKey() + "=\"" + e.getValue() + "\"")
+                            .collect(Collectors.joining(" "));
+
+            String label =
+                    "<<table "
+                            + attrs
+                            + ">"
+                            + "<tr><td colspan=\"2\">"
+                            + title
+                            + "</td></tr>"
+                            + formatRecursiveDataRow(data)
+                            + "</table>>";
+
+            var gnode = Factory.node(tnode.id.toString()).with(Shape.PLAIN).with(Label.raw(label));
+
+            g = g.with(gnode);
+
+            if (tnode instanceof TTagBase<?> ttag) {
+                g =
+                        g.with(
+                                Factory.node(ttag.sourceId.toString())
+                                        .link(Factory.to(Factory.node(ttag.id.toString()))));
+            }
+
+            if (tnode instanceof TEdgeBase<?, ?> tedge) {
+                g =
+                        g.with(
+                                Factory.node(tedge.id.toString())
+                                        .link(Factory.to(Factory.node(tedge.targetId.toString()))));
+            }
         }
-      }
 
-      Map<String, String> tableAttrs =
-          new HashMap<>(
-              Map.of(
-                  "border", "0",
-                  "cellborder", "1",
-                  "cellspacing", "0",
-                  "cellpadding", "4"));
-
-      String bgcolor = tnode.displayBackgroundColor();
-      if (!bgcolor.isEmpty()) {
-        tableAttrs.put("bgcolor", bgcolor);
-      }
-
-      String title = syms.get(tnode.id);
-
-      var attrs =
-          tableAttrs.entrySet().stream()
-              .map(e -> e.getKey() + "=\"" + e.getValue() + "\"")
-              .collect(Collectors.joining(" "));
-
-      String label =
-          "<<table "
-              + attrs
-              + ">"
-              + "<tr><td colspan=\"2\">"
-              + title
-              + "</td></tr>"
-              + formatRecursiveDataRow(data)
-              + "</table>>";
-
-      var gnode = Factory.node(tnode.id.toString()).with(Shape.PLAIN).with(Label.raw(label));
-
-      g = g.with(gnode);
-
-      if (tnode instanceof TTagBase<?> ttag) {
-        g =
-            g.with(
-                Factory.node(ttag.sourceId.toString())
-                    .link(Factory.to(Factory.node(ttag.id.toString()))));
-      }
-
-      if (tnode instanceof TEdgeBase<?, ?> tedge) {
-        g =
-            g.with(
-                Factory.node(tedge.id.toString())
-                    .link(Factory.to(Factory.node(tedge.targetId.toString()))));
-      }
+        return g;
     }
 
-    return g;
-  }
+    private String formatRecursiveData(Object data) {
+        if (data instanceof Map) {
+            @SuppressWarnings("unchecked")
+            Map<String, Object> m = (Map<String, Object>) data;
 
-  private String formatRecursiveData(Object data) {
-    if (data instanceof Map) {
-      @SuppressWarnings("unchecked")
-      Map<String, Object> m = (Map<String, Object>) data;
-
-      return "<table border=\"0\" cellborder=\"1\" cellspacing\"0\">"
-          + formatRecursiveDataRow(m)
-          + "</table>";
-    } else {
-      return HtmlEscapers.htmlEscaper().escape(data.toString());
+            return "<table border=\"0\" cellborder=\"1\" cellspacing\"0\">"
+                    + formatRecursiveDataRow(m)
+                    + "</table>";
+        } else {
+            return HtmlEscapers.htmlEscaper().escape(data.toString());
+        }
     }
-  }
 
-  private String formatRecursiveDataRow(Map<String, Object> data) {
-    StringBuilder sb = new StringBuilder();
-    for (var entry : data.entrySet()) {
-      sb.append("<tr>")
-          .append("<td align=\"right\"><b>")
-          .append(entry.getKey())
-          .append(":</b></td>")
-          .append("<td align=\"left\">")
-          .append(formatRecursiveData(entry.getValue()))
-          .append("</td>")
-          .append("</tr>");
+    private String formatRecursiveDataRow(Map<String, Object> data) {
+        StringBuilder sb = new StringBuilder();
+        for (var entry : data.entrySet()) {
+            sb.append("<tr>")
+                    .append("<td align=\"right\"><b>")
+                    .append(entry.getKey())
+                    .append(":</b></td>")
+                    .append("<td align=\"left\">")
+                    .append(formatRecursiveData(entry.getValue()))
+                    .append("</td>")
+                    .append("</tr>");
+        }
+        return sb.toString();
     }
-    return sb.toString();
-  }
 }
