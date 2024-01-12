63537bdc6893bb0dc92e92db831d04a30f22b5c8
Crutcher Dunnavant <crutcher@gmail.com>
2024-01-11 18:16:07 -0800

    DigestUtils tests.

--- java/src/main/java/loom/common/DigestUtils.java
+++ java/src/main/java/loom/common/DigestUtils.java
@@ -1,9 +1,20 @@
 package loom.common;
 
+import lombok.RequiredArgsConstructor;
+import loom.common.runtime.ExcludeFromJacocoGeneratedReport;
+
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 
-public class DigestUtils {
+@RequiredArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public final class DigestUtils {
+  public static final String MD5_ALGORITHM = "MD5";
+
+  /**
+   * Convert a byte array to a hex string.
+   * @param bytes The byte array to convert
+   * @return The hex string
+   */
   public static String bytesToHex(byte[] bytes) {
     StringBuilder hexString = new StringBuilder();
     for (byte b : bytes) {
@@ -16,14 +27,24 @@ public class DigestUtils {
     return hexString.toString();
   }
 
-  private static MessageDigest getMD5Digest() {
+  /**
+   * Get a MD5 MessageDigest instance.
+   * @return A MD5 MessageDigest instance
+   */
+  @ExcludeFromJacocoGeneratedReport
+  public static MessageDigest getMD5Digest() {
     try {
-      return MessageDigest.getInstance("MD5");
+      return MessageDigest.getInstance(MD5_ALGORITHM);
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
   }
 
+  /**
+   * Return the MD5 hash of a String as a hex String.
+   * @param str The String to hash
+   * @return The MD5 hash of the String as a hex String.
+   */
   public static String toMD5HexString(String str) {
     MessageDigest md = getMD5Digest();
     md.update(str.getBytes(StandardCharsets.UTF_8));
