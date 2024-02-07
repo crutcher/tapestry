538e5693ea5987427255a4bc9af487b02cca8ffa
Crutcher Dunnavant <crutcher@gmail.com>
2024-02-06 16:41:33 -0800

    Update code formatting in ZTensor and ZSpaceTestAssertions
    The commit accommodates modifications made to improve code readability by adjusting the indentations in ZTensor and ZSpaceTestAssertions. It includes adjustments addressing various constructs and methods, including the assertObjectJsonEquivalence method. Also, the inconsistent indentations in several methods of ZTensor have been fixed to enhance code readability and maintainability.

--- src/test/java/org/tensortapestry/zspace/experimental/ZSpaceTestAssertions.java
+++ src/test/java/org/tensortapestry/zspace/experimental/ZSpaceTestAssertions.java
@@ -7,19 +7,19 @@ import org.jetbrains.annotations.Nullable;
 import org.tensortapestry.zspace.impl.ZSpaceJsonUtil;
 
 public interface ZSpaceTestAssertions extends WithAssertions {
-    default void assertObjectJsonEquivalence(Object obj, String json) {
-        try {
-            assertThatJson(obj).isEqualTo(json);
-        } catch (AssertionError e) {
-            String objJson = ZSpaceJsonUtil.toPrettyJson(obj);
-            // We establish a 'clean' json by re-serializing the JSON derived object.
-            var objFromJson = ZSpaceJsonUtil.fromJson(json, obj.getClass());
-            var cleanJson = ZSpaceJsonUtil.toPrettyJson(objFromJson);
-            assertThatJson(objJson).isEqualTo(cleanJson);
-        }
+  default void assertObjectJsonEquivalence(Object obj, String json) {
+    try {
+      assertThatJson(obj).isEqualTo(json);
+    } catch (AssertionError e) {
+      String objJson = ZSpaceJsonUtil.toPrettyJson(obj);
+      // We establish a 'clean' json by re-serializing the JSON derived object.
+      var objFromJson = ZSpaceJsonUtil.fromJson(json, obj.getClass());
+      var cleanJson = ZSpaceJsonUtil.toPrettyJson(objFromJson);
+      assertThatJson(objJson).isEqualTo(cleanJson);
     }
+  }
 
-    default JsonAssert.ConfigurableJsonAssert assertThatJson(@Nullable Object actual) {
-        return JsonAssertions.assertThatJson(actual);
-    }
+  default JsonAssert.ConfigurableJsonAssert assertThatJson(@Nullable Object actual) {
+    return JsonAssertions.assertThatJson(actual);
+  }
 }
